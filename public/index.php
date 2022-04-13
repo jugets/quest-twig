d95189179f2d70ef66d62c8bc9f735bfc4d3b247
jugets <justine.gets@posteo.net>
2022-04-13 21:55:26 +0200

    Challenge heritage & inclusion

--- public/index.php
+++ public/index.php
@@ -5,5 +5,7 @@ require_once __DIR__ . '/../config/twig.php';
 
 
 $name = 'Wilder';
+$links = ['Home','Products'];
 
 echo $twig->render('home.html.twig', ['name' => $name]);
+echo $twig->render('layout.html.twig', ['links' => $links]);
